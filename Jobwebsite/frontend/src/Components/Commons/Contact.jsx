13b19053d9d22ee8da6b8c514234d1410316eeac
dagmawib <dagmawibelayneh14@gmail.com>
2023-11-14 16:34:20 +0300

    Contact us page update

--- Jobwebsite/frontend/src/Components/Commons/Contact.jsx
+++ Jobwebsite/frontend/src/Components/Commons/Contact.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React,{ useRef, useState } from 'react'
+import emailjs from '@emailjs/browser';
 import Heading from './Heading'
 import Footer from './Footer'
 import IMAGES from '../images'
 
 function Contact() {
+    const initialFormState = {
+        user_name: '',
+        user_email: '',
+        message: ''
+      };
+    
+      const [formData, setFormData] = useState(initialFormState);
+    
+      const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+      };
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+    e.preventDefault();
+    
+    emailjs.sendForm('service_c2srrug', 
+                     'template_zyh8pg8', 
+                      form.current, 'PwIUvH6qPZ9rTj5KO')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      setFormData(initialFormState);
+    };
+    
   return (
     <>
     <Heading />
@@ -30,31 +60,42 @@ function Contact() {
         
         <div className='contactForm'>
             <h2>Send a Message</h2>
+            <form ref={form} onSubmit={sendEmail}>
             <div className='formBox'>
                 <div className='inputBox w50'>
-                    <span>First Name</span>
-                    <input type="text" name='' required />
+                    <span>Full Name</span>
+                    <input type="text" 
+                           name="user_name" 
+                           required 
+                           value={formData.user_name}
+                           onChange={handleChange}
+                        />
                 </div>
-                <div className='inputBox w50'>
-                    <span>Last Name</span>
-                    <input type="text" name='' required />
-                </div>
+               
                 <div className='inputBox w50'>
                     <span>Email Address</span>
-                    <input type="email" name='' required />
+                    <input type="email" 
+                           name="user_email"  
+                           required 
+                           value={formData.user_email}
+                           onChange={handleChange}
+                        />
                 </div>
-                <div className='inputBox w50'>
-                    <span>Phone Number</span>
-                    <input type="text" name='' required />
-                </div>
+                
                 <div className='inputBox w100'>
                     <span>Write your message here...</span>
-                    <textarea type="text" name='' required />
+                    <textarea type="text"  
+                              name="message" 
+                              required 
+                              value={formData.message}
+                              onChange={handleChange}
+                            />
                 </div>
                 <div className='inputBox w100'>
-                    <input type="submit" value={"Send"} />
+                    <input type="submit" value="Send" />
                 </div>
             </div>
+          </form>
         </div>
     </div>
     </section>
